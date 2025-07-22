e3e5733a6f133e5f78ab40e87143b029d1fa2792
Tamirat Tezera <tamirattezera8@gmail.com>
2025-07-22 17:35:48 +0300

    Implement user authentication: login, logout, register with views and templates

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
 from django.views.generic.detail import DetailView
+from django.contrib.auth import login
 from .models import Library
 from .models import Book
 
@@ -13,3 +15,14 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # auto-login after registration
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'relationship_app/register.html', {'form': form})
