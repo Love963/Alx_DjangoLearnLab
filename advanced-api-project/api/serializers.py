2d13efa732d8ae0a657ba61af826d89cd0d19964
Tamirat Tezera <tamirattezera8@gmail.com>
2025-08-05 17:09:27 +0300

    Implement Author and Book models with nested custom serializers and validation

--- advanced-api-project/api/serializers.py
+++ advanced-api-project/api/serializers.py
@@ -1,20 +1,23 @@
 from rest_framework import serializers
 from datetime import datetime
+from rest_framework.exceptions import ValidationError
+
 from .models import Author, Book
 
+# Serializes all Book fields and validates that publication_year is not in the future.
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = ['id', 'title', 'publication_year', 'author']
 
+    def validate_publication_year(self, value):
+        """Ensure the publication year is not in the future."""
+        current_year = datetime.now().year
+        if value > current_year:
+            raise ValidationError("Publication year cannot be in the future.")
+        return value
 
-        def validate_publication_year(self, value):
-            """Ensure the publication year is not in the future."""
-            current_year = datetime.now().year
-            if value > current_year:
-                raise serializers.validationError("Publication year cannot be in the future.")
-            return value
-# Represents a writer. Has a one-to-many relationship with Book.
+# Serializes the author's name and uses a nested BookSerializer to include related books.
 class AuthorSerializer(serializers.ModelSerializer):  # 
     books = BookSerializer(many=True, read_only = True)  # Nested representation
     class Meta:
