86c6e3b85463a39408b899783df21b7f626c8eb7
Tamirat Tezera <tamirattezera8@gmail.com>
2025-08-09 13:32:59 +0300

    Unit Tests for Django REST Framework APIs

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,7 +1,7 @@
 from rest_framework import generics, permissions
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from rest_framework import generics, filters
-# from django_filters.rest_framework import DjangoFilterBackend
+from django_filters.rest_framework import DjangoFilterBackend
 from django_filters import rest_framework
 
 from rest_framework.response import Response
--- advanced-api-project/ecommerce_project/users/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from rest_framework import generics
-from .serializers import UserRegistrationSerializer
-from .models import CustomUser
-# Create your views here.
-class RegisterView(generics.CreateAPIView):
-    queryset = CustomUser.objects.all()
-    serializer_class = UserRegistrationSerializer
-    
