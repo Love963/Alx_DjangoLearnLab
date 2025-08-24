8336f8725dd3a53c317e4693a8fd30c0a23efb08
Tamirat Tezera <tamirattezera8@gmail.com>
2025-08-24 21:44:56 +0300

    Prepare project for production deployment with production settings, tests, and README documentation

--- social_media_api/posts/tests.py
+++ social_media_api/posts/tests.py
@@ -1,54 +1,53 @@
-# posts/tests.py
-from django.test import TestCase
-from rest_framework.test import APIClient
+from django.urls import reverse
 from rest_framework import status
-from django.contrib.auth import get_user_model
+from rest_framework.test import APITestCase
+from accounts.models import User
 from .models import Post, Comment
 
-User = get_user_model()
+class PostsCommentsFeedTests(APITestCase):
 
-class PostsCommentsFeedTests(TestCase):
     def setUp(self):
-        self.user1 = User.objects.create_user(username="user1", password="pass1234")
-        self.user2 = User.objects.create_user(username="user2", password="pass1234")
-        self.client = APIClient()
-        self.client.force_authenticate(user=self.user1)
+        self.user = User.objects.create_user(username='user1', password='pass123')
+        self.user2 = User.objects.create_user(username='user2', password='pass123')
+        self.client.login(username='user1', password='pass123')
 
-        self.posts_url = "/posts/"
-        self.comments_url = "/comments/"
-        self.feed_url = "/feed/feed/"
-
-        self.post = Post.objects.create(author=self.user1, title="Post1", content="Content1")
-        self.comment = Comment.objects.create(author=self.user1, post=self.post, content="Nice post!")
+        # Create a sample post
+        self.post = Post.objects.create(author=self.user, title="Test Post", content="Some content")
+        # Create a sample comment
+        self.comment = Comment.objects.create(post=self.post, author=self.user, content="Nice post!")
 
     def test_create_post(self):
-        data = {"title": "New Post", "content": "New Content"}
-        response = self.client.post(self.posts_url, data)
+        url = reverse('post-list')  
+        data = {'title': 'New Post', 'content': 'Content of new post'}
+        response = self.client.post(url, data, format='json', follow=True) 
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
     def test_post_update_permission(self):
-        post2 = Post.objects.create(author=self.user2, title="Other Post", content="Other content")
-        url = f"{self.posts_url}{post2.id}/"
-        data = {"title": "Hacked", "content": "Hacked content"}
-        response = self.client.put(url, data)
+        self.client.logout()
+        self.client.login(username='user2', password='pass123')
+        url = reverse('post-detail', args=[self.post.id]) 
+        data = {'title': 'Hacked Post'}
+        response = self.client.put(url, data, format='json', follow=True)
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
     def test_create_comment(self):
-        data = {"post": self.post.id, "content": "Another comment"}
-        response = self.client.post(self.comments_url, data)
+        url = reverse('comment-list') 
+        data = {'post': self.post.id, 'content': 'Great!'}
+        response = self.client.post(url, data, format='json', follow=True)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
     def test_comment_update_permission(self):
-        comment2 = Comment.objects.create(author=self.user2, post=self.post, content="Other comment")
-        url = f"{self.comments_url}{comment2.id}/"
-        data = {"content": "Hacked comment"}
-        response = self.client.put(url, data)
+        self.client.logout()
+        self.client.login(username='user2', password='pass123')
+        url = reverse('comment-detail', args=[self.comment.id]) 
+        data = {'content': 'Edited by someone else'}
+        response = self.client.put(url, data, format='json', follow=True)
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
     def test_feed_posts(self):
-        self.user1.following.add(self.user2)
-        post2 = Post.objects.create(author=self.user2, title="Followed Post", content="Content2")
-        response = self.client.get(self.feed_url)
+        self.user2.following.add(self.user)
+        self.client.logout()
+        self.client.login(username='user2', password='pass123')
+        url = reverse('feed')  
+        response = self.client.get(url, follow=True)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(response.data), 1)
-        self.assertEqual(response.data[0]["title"], "Followed Post")
